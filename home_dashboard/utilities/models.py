7be5d02318b92386d36ea4cb0088eafb23549f50
Rob van Loon <borrob@me.com>
2018-03-10 21:02:08 +0100

    Added meter readings

--- home_dashboard/utilities/models.py
+++ home_dashboard/utilities/models.py
@@ -7,3 +7,12 @@ class Meter(models.Model):
     
     def __str__(self):
         return('Meter ' + self.meter_name + ' with unit: ' + self.meter_unit)
+
+class Reading(models.Model):
+    date = models.DateField()
+    reading = models.DecimalField(max_digits=10, decimal_places=2)
+    meter = models.ForeignKey('Meter', on_delete=models.CASCADE)
+    remark = models.CharField(max_length=255)
+
+    def __str__(self):
+        return('Reading: {0} {1} ({2} - {3}'.format(self.reading, self.meter.meter_unit, self.date, self.meter.meter_name))
--- home_dashboard/utilities/urls.py
+++ home_dashboard/utilities/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('meterlist', views.ListMeters.as_view(), name='meter_list'),
     path('addmeter', views.add_meter, name='add_meter'),
     path('deletemeter', views.delete_meter, name='delete_meter'),
-    path('editmeter', views.edit_meter, name='edit_meter')
+    path('editmeter', views.edit_meter, name='edit_meter'),
+    path('readinglist', views.ListReadings.as_view(), name='reading_list')
 ]
