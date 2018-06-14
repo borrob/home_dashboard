cf3f45d16b37dabf37f05c2c9b85434a5bc8a0f6
Rob van Loon <borrob@me.com>
2018-06-15 00:42:19 +0200

    Add extra testing to work with signals.

--- home_dashboard/utilities/signals.py
+++ home_dashboard/utilities/signals.py
@@ -3,9 +3,6 @@ Supplies the signals for the utility app.
 """
 from .models import Reading, update_usage_after_new_reading
 
-import pdb
-
-
 def reading_saved(sender, instance, **kwargs): # pylint: disable=unused-argument
     """
     Calculate the new usage when a reading is saved.
