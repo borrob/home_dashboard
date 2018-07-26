61f559e25f8545007b0343ce8051f56e026a07cf
Rob van Loon <borrob@me.com>
2018-07-26 23:47:28 +0200

    WIP: setup logging

--- home_dashboard/dashboard/views.py
+++ home_dashboard/dashboard/views.py
@@ -4,6 +4,11 @@ Defining the dashboard views: URL links and their responses.
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger('home_dashboard_log')
 
 @login_required
 def index(request):
@@ -17,5 +22,11 @@ def profile(request):
     """
     Show the user profile.
     """
+    logger.error('running! from the dashboard view')
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warning message')
+    logger.error('error message')
+    logger.critical('oopsie')
     version = settings.VERSION
     return render(request, 'dashboard/profile.html', {'version': version})
