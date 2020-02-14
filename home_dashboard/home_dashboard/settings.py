c26f255c626b4020ab841eea91260660f3555320
Rob van Loon <borrob@me.com>
2020-02-14 21:55:43 +0100

    Set version

--- home_dashboard/home_dashboard/settings.py
+++ home_dashboard/home_dashboard/settings.py
@@ -152,7 +152,7 @@ LOGIN_REDIRECT_URL = '/dashboard'
 
 ##PROJECT SPECIFIC
 PAGE_SIZE = 10
-VERSION = '0.6-5-g533f835'
+VERSION = '0.7.1-1-gbb87be8'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
